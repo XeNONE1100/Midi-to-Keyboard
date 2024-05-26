f35ee607ebe3c0dcc97019e10a46e35f0869cf04
XeNONE1100 <priinteemps@gmail.com>
2024-05-26 20:22:09 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,24 @@
+
+
+# arturia is the keyboard i used myself, check the readme for how to change it to your own
+client_name = "Arturia MiniLab mkII"
+
+
+#letter example:
+midi_to_keyboard = {
+"53," : "f",
+"55," : "g",
+"57," : "h",
+"59," : "j",
+}
+
+#arrow key example:
+
+# }
+# midi_to_keyboard = {
+# "53," : Key.up,
+# "55," : Key.down,
+# "57," : Key.left,
+# "59," : Key.right,
+# }
+
---
+++ start.py
@@ -0,0 +1,27 @@
+from subprocess import Popen, PIPE, CalledProcessError
+from pynput.keyboard import Controller as KeyController
+from pynput.keyboard import Key
+
+
+from config import *
+
+cmd = "aseqdump", '-p', client_name 
+
+
+keyboard = KeyController()
+
+with Popen(cmd, stdout=PIPE, bufsize=1, universal_newlines=True) as p:
+    for line in p.stdout:
+        # print(line[40:43])   # UNCOMMENT THIS LINE FOR DETECTING WHICH KEY IS WHICH NUMBER
+        try:
+            if line[8:].startswith("Note on"):
+                    keyboard.press(midi_to_keyboard[line[40:43]])
+            
+            elif line[8:].startswith("Note off"):
+                    keyboard.release(midi_to_keyboard[line[40:43]])
+
+        except:
+            pass
+
+if p.returncode != 0:
+    raise CalledProcessError(p.returncode, p.args)
