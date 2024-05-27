d342543fadef157e2cb5733d9824276501c3c685
XeNONE1100 <priinteemps@gmail.com>
2024-05-27 09:10:16 +0200

    added a mixed example in config.py

--- config.py
+++ config.py
@@ -4,13 +4,20 @@ from pynput.keyboard import Key
 client_name = "Arturia MiniLab mkII"
 
 
-# letter example:
-midi_to_keyboard = {
-"53," : "f",
-"55," : "g",
-"57," : "h",
-"59," : "j",
-}
+# letter only examples:
+# midi_to_keyboard = {
+# "53," : "f",
+# "55," : "g",
+# "57," : "h",
+# "59," : "j",
+# }
+# 
+# midi_to_keyboard = {
+# "36," : "f",
+# "37," : "g",
+# "38," : "h",
+# "39," : "j",
+# }
 
 #arrow key example:
 
@@ -23,23 +30,30 @@ midi_to_keyboard = {
 # }
 
 
-# midi_to_keyboard = {
-# "36," : "1",
-# "37," : "2",
+#mixed example 
 
-# "48," : Key.shift,
-# "53," : Key.space,
-# "60," : Key.backspace,
-# "50," : "z",
-# "52," : "x",
-# "55,," : "c",
+midi_to_keyboard = {
+"36," : "1",
+"37," : "2",
+"39," : "4",
 
+"48," : Key.shift,
+"53," : Key.space,
+"60," : Key.backspace,
+"50," : "z",
+"52," : "x",
+"55," : "c",
 
-# "69," : Key.left,
-# "70," : Key.up,
-# "71," : Key.down,
-# "72," : Key.right,
-# }
+"41," : Key.page_up,
+# "42," : Key.end,
+"43," : Key.page_down,
+"38," : Key.home,
+
+"69," : Key.left,
+"70," : Key.up,
+"71," : Key.down,
+"72," : Key.right,
+}
 
 
 
